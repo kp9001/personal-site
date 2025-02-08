d57e41409c81d09ba06bf47c442c27bf60414217
Kendra Plante <kendra@kplante.com>
2025-02-08 12:27:57 -0500

    update build script and gitignore to support local builds

--- cloudflare.sh
+++ cloudflare.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-set -eou pipefail
+rm ./*.tar.gz 2>/dev/null
 
 wget $( curl -L -s https://api.github.com/repos/gohugoio/hugo/releases/latest | grep "browser_download_url.*linux-amd64.tar.gz" | grep -v "extended" | cut -d\" -f4 )
-tar xvf ./*.tar.gz
+tar -xvzf ./*.tar.gz hugo
 chmod u+x ./hugo
 
 ./hugo --minify
