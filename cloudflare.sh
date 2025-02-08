8f3072e1def9009f0cc3990dadc92f147b296ea2
Kendra Plante <kendra@kplante.com>
2025-02-07 23:20:19 -0500

    remove commented version pin from build command

--- cloudflare.sh
+++ cloudflare.sh
@@ -2,7 +2,6 @@
 
 rm ./*.tar.gz
 wget $( curl -L -s https://api.github.com/repos/gohugoio/hugo/releases/latest | grep "browser_download_url.*linux-amd64.tar.gz" | grep -v "extended" | cut -d\" -f4 )
-#wget https://github.com/gohugoio/hugo/releases/download/v0.119.0/hugo_0.119.0_Linux-64bit.tar.gz
 tar xvf ./*.tar.gz
 chmod u+x ./hugo
 ./hugo --minify
