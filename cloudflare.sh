f2d22012b8c342cf5f5e85ba9e3b16e06528426b
Kendra Plante <kendra@kplante.com>
2025-02-07 23:37:14 -0500

    remove unnecessary rm from build script

--- cloudflare.sh
+++ cloudflare.sh
@@ -2,7 +2,6 @@
 
 set -eou pipefail
 
-rm ./*.tar.gz
 wget $( curl -L -s https://api.github.com/repos/gohugoio/hugo/releases/latest | grep "browser_download_url.*linux-amd64.tar.gz" | grep -v "extended" | cut -d\" -f4 )
 tar xvf ./*.tar.gz
 chmod u+x ./hugo
