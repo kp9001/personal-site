0188a1f86e75ed40f96a671137db19720175c790
Kendra Plante <kendra@kplante.com>
2025-02-07 23:31:51 -0500

    set pipefail on build script

--- cloudflare.sh
+++ cloudflare.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+set -eou pipefail
+
 rm ./*.tar.gz
 wget $( curl -L -s https://api.github.com/repos/gohugoio/hugo/releases/latest | grep "browser_download_url.*linux-amd64.tar.gz" | grep -v "extended" | cut -d\" -f4 )
 tar xvf ./*.tar.gz
 chmod u+x ./hugo
+
 ./hugo --minify
